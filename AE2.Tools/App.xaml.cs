6fdf22a190d126628eecef7d5dfb4b4902d44581
DevInCube <ruslan.hadyniak@gmail.com>
2015-03-02 02:58:56 +0200

    tools resources repacker added

---
+++ AE2.Tools/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using AE2.Tools.Loaders;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AE2.Tools
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            ResourceLoader.loadResourcesPak(null);
+            ResourceLoader.saveUnpackedResources(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "pak"));
+            Environment.Exit(0);
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            //
+        }
+    }
+}
---
+++ AE2.Tools/Loaders/ResourceLoader.cs
@@ -0,0 +1,81 @@
+﻿using AE2.Tools.Properties;
+using java.io;
+using java.lang;
+
+namespace AE2.Tools.Loaders
+{
+    public static class ResourceLoader
+    {
+        private static String[] resourcesNames;
+        private static byte[][] resourcesData;
+
+        public static  void loadResourcesPak(String pakFileName)
+        {
+            if (resourcesNames == null)
+            {
+                resourcesNames = null;
+                int[] arrayOfInt1 = null;
+                int[] arrayOfInt2 = null;
+                InputStream stream = ResourceLoader.getResourceAsStream("Resources/1.pak");
+                DataInputStream resStream = new DataInputStream(stream);
+                int i = resStream.readShort();
+                int resLength = resStream.readShort();
+                resourcesNames = new String[resLength];
+                arrayOfInt1 = new int[resLength];
+                arrayOfInt2 = new int[resLength];
+                for (int k = 0; k < resLength; k++)
+                {
+                    resourcesNames[k] = resStream.readUTF();
+                    arrayOfInt1[k] = (resStream.readInt() + i);
+                    arrayOfInt2[k] = resStream.readShort();
+                }
+                resourcesData = new byte[resourcesNames.Length][];
+                for (int m = 0; m < resourcesNames.Length; m++)
+                {
+                    resourcesData[m] = new byte[arrayOfInt2[m]];
+                    resStream.readFully(resourcesData[m]);
+                }
+                resStream.close();
+            }
+        }
+
+        public static void saveUnpackedResources(string dir)
+        {
+            for (int i = 0; i < resourcesNames.Length;i++ )
+            {
+                String key = resourcesNames[i];
+                string path = System.IO.Path.Combine(dir, key.ToString());
+                System.IO.File.WriteAllBytes(path, resourcesData[i]);
+            }
+        }
+
+        private static InputStream getResourceAsStream(string path)
+        {
+            System.IO.MemoryStream ms = new System.IO.MemoryStream();
+            using (System.IO.FileStream file = new System.IO.FileStream(path, System.IO.FileMode.Open, System.IO.FileAccess.Read))
+            {
+                byte[] bytes = new byte[file.Length];
+                file.Read(bytes, 0, (int)file.Length);
+                ms.Write(bytes, 0, (int)file.Length);
+            }
+            return new DataInputStream(ms);
+        }
+
+        public static  InputStream getResourceStream(String resName)
+        {
+            return new ByteArrayInputStream(getResourceData(resName));
+        }
+
+        public static  byte[] getResourceData(String resName)
+        {
+            for (int i = 0; i < resourcesNames.Length; i++)
+            {
+                if (resName.Equals(resourcesNames[i]))
+                {
+                    return resourcesData[i];
+                }
+            }
+            return null;
+        }
+    }
+}
