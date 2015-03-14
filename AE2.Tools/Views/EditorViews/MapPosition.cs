d2ed9f88be34be78a176bc8e5e4b1d0bf57cbc23
devincube <ruslan.hadyniak@gmail.com>
2015-03-14 23:34:07 +0200

    island generation algorithm

---
+++ AE2.Tools/Views/EditorViews/MapPosition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AE2.Tools.Views.EditorViews
+{
+    public struct MapPosition
+    {
+
+        public sbyte X;
+        public sbyte Y;
+
+        public MapPosition(MapPosition p)
+        {
+            X = p.X;
+            Y = p.Y;
+        }
+
+        public MapPosition(sbyte x, sbyte y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public MapPosition(int x, int y) : this((sbyte)x, (sbyte)y) { }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is MapPosition)) return false;
+            MapPosition o = (MapPosition)obj;
+            return (o.X == X && o.Y == Y);            
+        }
+
+        public bool IsWithin(sbyte x, sbyte y, sbyte w, sbyte h)
+        {
+            return (X >= x && X < (x + w)) && (Y >= y && Y < (y + h));
+        }
+
+        public bool IsWithin(int x, int y, int w, int h)
+        {
+            return IsWithin((sbyte)x, (sbyte)y, (sbyte)w, (sbyte)h);
+        }
+    }
+}
