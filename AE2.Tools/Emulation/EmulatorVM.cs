0a009d885a63b92d8dae2e6044180d8460a8b9e1
DevInCube <ruslan.hadyniak@gmail.com>
2023-09-03 22:17:23 +0300

    EmulatorVM refactor

--- AE2.Tools/Emulation/EmulatorVM.cs
+++ AE2.Tools/Emulation/EmulatorVM.cs
@@ -10,32 +10,12 @@ using System.Windows;
 
 namespace AE2.Tools.Emulation
 {
-    public class KeyCommand
-    {
-        public Key Key { get; set; }
-        public ICommand Command { get; set; }
-
-        public KeyCommand(Key key, Action command)
-        {
-            this.Key = key;
-            this.Command = new SimpleCommand(command);
-        }
-    }
-
     public class EmulatorVM : ObservableObject, IEventSource
     {
-        public event Action<int> KeyPressed;
-        public event Action<int> KeyReleased;
-        public event Action<System.Drawing.Point> PointerMoved;
-        public event Action<System.Drawing.Point> PointerPressed;
-        public event Action<System.Drawing.Point> PointerReleased;
-
-        public event Action ClosedAction;
-        public Dictionary<string, KeyCommand> KeyDict { get; set; }
-
         private IEventSource _eventSource;
-
         private System.Windows.Controls.Control _control;
+
+        public Dictionary<string, KeyCommand> KeyDict { get; set; }
 
         public System.Windows.Controls.Control Control
         {
@@ -43,63 +23,68 @@ namespace AE2.Tools.Emulation
             set => SetProperty(ref _control, value);
         }
 
+        public event Action<int> KeyPressed;
+        public event Action<int> KeyReleased;
+        public event Action<System.Drawing.Point> PointerMoved;
+        public event Action<System.Drawing.Point> PointerPressed;
+        public event Action<System.Drawing.Point> PointerReleased;
+        public event Action ClosedAction;
+
         public EmulatorVM()
         {
             KeyDict = new Dictionary<string, KeyCommand>
             {
                 {"L", new KeyCommand(Key.F1, () => { OnKeyPressed(-6); })},
                 {"R", new KeyCommand(Key.F2, () => { OnKeyPressed(-7); })},
-                {"0", new KeyCommand(Key.NumPad0, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM0); })},
-                {"1", new KeyCommand(Key.NumPad7, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM1); })},
-                {"2", new KeyCommand(Key.NumPad8, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM2); })},
-                {"3", new KeyCommand(Key.NumPad9, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM3); })},
-                {"4", new KeyCommand(Key.NumPad4, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM4); })},
-                {"5", new KeyCommand(Key.NumPad5, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM5); })},
-                {"6", new KeyCommand(Key.NumPad6, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM6); })},
-                {"7", new KeyCommand(Key.NumPad1, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM7); })},
-                {"8", new KeyCommand(Key.NumPad2, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM8); })},
-                {"9", new KeyCommand(Key.NumPad3, () => { OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM9); })}
+                {"0", new KeyCommand(Key.NumPad0, () => { OnKeyPressed(Canvas.KEY_NUM0); })},
+                {"1", new KeyCommand(Key.NumPad7, () => { OnKeyPressed(Canvas.KEY_NUM1); })},
+                {"2", new KeyCommand(Key.NumPad8, () => { OnKeyPressed(Canvas.KEY_NUM2); })},
+                {"3", new KeyCommand(Key.NumPad9, () => { OnKeyPressed(Canvas.KEY_NUM3); })},
+                {"4", new KeyCommand(Key.NumPad4, () => { OnKeyPressed(Canvas.KEY_NUM4); })},
+                {"5", new KeyCommand(Key.NumPad5, () => { OnKeyPressed(Canvas.KEY_NUM5); })},
+                {"6", new KeyCommand(Key.NumPad6, () => { OnKeyPressed(Canvas.KEY_NUM6); })},
+                {"7", new KeyCommand(Key.NumPad1, () => { OnKeyPressed(Canvas.KEY_NUM7); })},
+                {"8", new KeyCommand(Key.NumPad2, () => { OnKeyPressed(Canvas.KEY_NUM8); })},
+                {"9", new KeyCommand(Key.NumPad3, () => { OnKeyPressed(Canvas.KEY_NUM9); })}
             };
-
         }
 
         public void OnMouseMoved(System.Drawing.Point pos)
         {
-            if (PointerMoved != null)
-                PointerMoved(pos);
+            PointerMoved?.Invoke(pos);
         }
 
         public void OnMouseDown(System.Drawing.Point pos)
         {
-            if (PointerPressed != null)
-                PointerPressed(pos);
+            PointerPressed?.Invoke(pos);
         }
 
         public void OnMouseUp(System.Drawing.Point pos)
         {
-            if (PointerReleased != null)
-                PointerReleased(pos);
+            PointerReleased?.Invoke(pos);
         }
 
         private void OnKeyPressed(int code)
         {
-            if (KeyPressed == null || KeyReleased == null) return;
-            this.KeyPressed(code);
+            if (KeyPressed == null || KeyReleased == null) 
+                return;
+
+            KeyPressed(code);
             System.Threading.Thread.Sleep(40);
-            this.KeyReleased(code);
+            KeyReleased(code);
         }
 
         public void SetEventSource(IEventSource eventSource)
         {
-            this._eventSource = eventSource;
+            _eventSource = eventSource;
         }
 
-        internal void LoadMIDlet(MIDlet midlet)
+        public void LoadMIDlet(MIDlet midlet)
         {
             midlet.getClass().setResourceLoader(new WPFResourceLoader("Resources"));
             midlet.Destroyed += midlet_Destroyed;
             Display display = Display.getDisplay(midlet);
-            this.Control = display.Control;
+            Control = display.Control;
             ((DisplayControl) display.Control).SetEventSource(_eventSource);
             midlet.startApp();
         }
@@ -113,8 +98,7 @@ namespace AE2.Tools.Emulation
         {
             Application.Current.Dispatcher.Invoke(() =>
             {
-                if (ClosedAction != null)
-                    ClosedAction();
+                ClosedAction?.Invoke();
             });
         }
     }
---
+++ AE2.Tools/Emulation/KeyCommand.cs
@@ -0,0 +1,18 @@
+﻿using MIDP.WPF.ViewModels;
+using System;
+using System.Windows.Input;
+
+namespace AE2.Tools.Emulation
+{
+    public class KeyCommand
+    {
+        public Key Key { get; set; }
+        public ICommand Command { get; set; }
+
+        public KeyCommand(Key key, Action command)
+        {
+            Key = key;
+            Command = new SimpleCommand(command);
+        }
+    }
+}
