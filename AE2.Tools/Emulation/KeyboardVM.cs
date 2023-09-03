c9ba5f4cc8c418dac2afc4cccb42f3e9f7cf3a61
DevInCube <ruslan.hadyniak@gmail.com>
2023-09-03 22:19:59 +0300

    refactored KeyboardVM

--- AE2.Tools/Emulation/KeyboardVM.cs
+++ AE2.Tools/Emulation/KeyboardVM.cs
@@ -7,14 +7,7 @@ namespace AE2.Tools.Emulation
 {
     public class KeyboardVM : ObservableObject, IEventSource
     {
-
         public string CustomCode { get; set; }
-
-        public event Action<int> KeyPressed;
-        public event Action<int> KeyReleased;
-        public event Action<System.Drawing.Point> PointerMoved;
-        public event Action<System.Drawing.Point> PointerPressed;
-        public event Action<System.Drawing.Point> PointerReleased;
 
         public ICommand CustomKey { get; set; }
         public ICommand KeyL { get; set; }
@@ -28,77 +21,45 @@ namespace AE2.Tools.Emulation
         public ICommand Key7 { get; set; }
         public ICommand Key8 { get; set; }
         public ICommand Key9 { get; set; }
-        public ICommand Key0 { get; set; }        
+        public ICommand Key0 { get; set; }
+
+        public event Action<int> KeyPressed;
+        public event Action<int> KeyReleased;
+        public event Action<System.Drawing.Point> PointerMoved;
+        public event Action<System.Drawing.Point> PointerPressed;
+        public event Action<System.Drawing.Point> PointerReleased;
 
         public KeyboardVM()
         {            
             CustomKey = new SimpleCommand(() =>
             {
-                var code = 0;
-                if (int.TryParse(CustomCode, out code))
+                if (int.TryParse(CustomCode, out var code))
                 {
                     OnKeyPressed(code);
                 }
             });
-            KeyL = new SimpleCommand(() =>
-            {
-                OnKeyPressed(-6);
-            });
-            KeyR = new SimpleCommand(() =>
-            {
-                OnKeyPressed(-7);
-            });
-            Key1 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM1);
-            });
-            Key2 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM2);
-            });
-            Key3 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM3);
-            });
-            Key4 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM4);
-            });
-            Key5 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM5);
-            });
-            Key6 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM6);
-            });
-            Key7 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM7);
-            });
-            Key8 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM8);
-            });
-            Key9 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM9);
-            });
-            Key0 = new SimpleCommand(() =>
-            {
-                OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM0);
-            });
+            KeyL = new SimpleCommand(() => OnKeyPressed(-6));
+            KeyR = new SimpleCommand(() => OnKeyPressed(-7));
+            Key1 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM1));
+            Key2 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM2));
+            Key3 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM3));
+            Key4 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM4));
+            Key5 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM5));
+            Key6 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM6));
+            Key7 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM7));
+            Key8 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM8));
+            Key9 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM9));
+            Key0 = new SimpleCommand(() => OnKeyPressed(javax.microedition.lcdui.Canvas.KEY_NUM0));
         }
 
         private void OnKeyPressed(int code)
         {
-            if (KeyPressed == null || KeyReleased == null) return;
-            this.KeyPressed(code);
+            if (KeyPressed == null || KeyReleased == null) 
+                return;
+
+            KeyPressed(code);
             Thread.Sleep(40);
-            this.KeyReleased(code);
+            KeyReleased(code);
         }
-
-
-        
     }
 }
